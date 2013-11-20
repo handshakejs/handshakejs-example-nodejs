753e30b9e5c76f34688315559fdb7e68bcd61749
scottmotte <scott@scottmotte.com>
2013-11-20 12:49:06 -0800

    Core of example app using handshake-js

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
 var express = require('express');
-var app = express();
+var app     = express();
+var PORT    = process.env.PORT || 3000; 
 
-app.listen(3000);
-console.log('Listening on port 3000');
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({secret: 'secret_stuff. you should change this value.'}));
+
+app.get('/', function(req, res) {
+  res.render('index.jade', {session: req.session});
+});
+
+app.get('/logout', function(req, res) {
+  req.session.user = null;
+  res.redirect('/dashboard');
+});
+
+app.get('/dashboard', function(req, res) {
+  if (!req.session.user) {
+    res.redirect('/');
+  } else {
+    res.render('dashboard.jade', {session: req.session});
+  }
+});
+
+app.post('/login/success', function(req, res) {
+  req.session.user = req.body.email;
+  res.redirect('/dashboard');
+});
+
+app.listen(PORT);
+console.log('Listening on port '+PORT);
